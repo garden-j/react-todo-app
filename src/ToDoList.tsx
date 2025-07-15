8532e6f5e98e4d2337b09d6c036ea8aadef12eaa
garden <garden5479@gmail.com>
2025-07-15 14:28:16 +0900

    add to do

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,16 +1,76 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  atom,
+  useRecoilState,
+  useRecoilValue,
+  useResetRecoilState,
+  useSetRecoilState,
+} from "recoil";
+
+interface IForm {
+  toDo: string;
+}
+
+interface IToDO {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDO[]>({
+  key: "toDo",
+  default: [],
+});
 
 function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "Pleas write a To Do",
+          })}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+/* function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    setToDoError("");
     setToDo(toDo);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo.length < 10) {
+        return setToDoError("To do should be longer");
+    }
+    console.log("submit");
   };
   return (
     <div>
@@ -20,6 +80,6 @@ function ToDoList() {
       </form>
     </div>
   );
-}
+} */
 
 export default ToDoList;
