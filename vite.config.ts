0cea191e445b32a719fdff22545ddfaa2fab9ebb
garden <garden5479@gmail.com>
2025-07-16 21:10:38 +0900

    setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/react-masterclass/react-todo-app/", // <-- 여기에 꼭 추가
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
 });
