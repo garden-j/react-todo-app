2d4aa2b7bee8cd8e8ee5a6bcab217e9774e4ebca
garden <garden5479@gmail.com>
2025-07-16 21:04:26 +0900

    publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-masterclass/react-todo-app/", // <-- 여기에 꼭 추가
   plugins: [react()],
-})
+});
